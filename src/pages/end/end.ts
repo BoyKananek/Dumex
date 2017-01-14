eebd661bde347ccb74a93916dfd3148034561df8
Kan-anek <kan-anek.a@burda.co.th>
2017-01-15 01:49:49 +0700

    finish intro page

---
+++ src/pages/end/end.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { NavController,App } from 'ionic-angular';
+
+
+@Component({
+  selector: 'page-end',
+  templateUrl: 'end.html'
+})
+export class EndPage {
+
+  constructor(public navCtrl: NavController,public app:App) {
+    
+  }
+  
+
+}
--- src/pages/intro/intro.ts
+++ src/pages/intro/intro.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'page-intro',
@@ -8,8 +9,47 @@ import { NavController } from 'ionic-angular';
 })
 export class IntroPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,public http: Http) {
     
+  }
+  digital101(){
+      console.log('You are Digital 101');
+      var data: any = {
+          type: 'Digital 101'
+      }
+      this.http.post('https://dumex.herokuapp.com/api/save', data)
+      .subscribe(data=>{
+          //go to next page with data.type
+      },error =>{
+          console.log('Error');
+      })
+      
+
+  }
+  digital201(){
+      var data: any ={
+          type : 'Digital 201'
+      }
+      this.http.post('https://dumex.herokuapp.com/api/save', data)
+      .subscribe(data=>{
+          //go to next page;
+      },error =>{
+          console.log('Error');
+      })
+
+      console.log('You are Digital 201');
+  }
+  digital301(){
+      var data: any = {
+          type: 'Digital 301'
+      }
+      this.http.post('https://dumex.herokuapp.com/api/save', data)
+      .subscribe(data=>{
+          //go to next page;
+      },error =>{
+          console.log('Error');
+      })
+      console.log('You are Digital 301');
   }
 
 }
